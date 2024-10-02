2b958ed618bd867184d1ba233f74aa41c2003d7b
carthem <ceserranoj@outlook.com>
2024-10-02 20:21:41 +0200

    actualizacion

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,20 +3,21 @@ let currentSlide = 0;
 function moveSlide(n) {
     const slides = document.querySelector('.carousel-slide');
     const totalSlides = slides.children.length;
-
-    // Actualiza el slide actual
-    currentSlide += n;
-
-    // Si estamos en el último slide, vuelve al primero
-    if (currentSlide >= totalSlides) {
-        currentSlide = 0;
-    }
-
-    // Si estamos en el primero y nos movemos hacia atrás, vuelve al último
-    if (currentSlide < 0) {
-        currentSlide = totalSlides - 1;
-    }
-
-    // Mueve las diapositivas
+    currentSlide = (currentSlide + n + totalSlides) % totalSlides;
     slides.style.transform = `translateX(-${currentSlide * 100}%)`;
 }
+
+function showCategory(categoryId) {
+    // Oculta todas las secciones
+    var contents = document.getElementsByClassName('category-content');
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].style.display = 'none';
+    }
+    
+    // Muestra la sección seleccionada
+    var selectedCategory = document.getElementById(categoryId);
+    if (selectedCategory) {
+        selectedCategory.style.display = 'block';
+    }
+}
+
